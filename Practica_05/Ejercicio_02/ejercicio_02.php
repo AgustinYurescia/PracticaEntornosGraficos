d4006d025edf88fde92ecd153e19049e2d1219c4
AgustinYurescia <agustin_yurescia@hotmail.com>
2020-06-06 14:58:24 -0300

    Práctica 5

--- Practica_05/Ejercicio_02/ejercicio_02.php
+++ Practica_05/Ejercicio_02/ejercicio_02.php
@@ -4,10 +4,6 @@
     $asunto = $_POST['asunto'];
     $cuerpo = $_POST['consulta'];
     $mail = $_POST['mail'];
-<<<<<<< HEAD
-    echo $asunto;
-=======
->>>>>>> 4b777b6cbba934d6f70823a27d15d76e646d71e8
     $headers = "From: ".$nombreyap." <".$mail.">\r\n";
     mail($destinatario, $asunto, $cuerpo, $headers);
 ?>
